602ab8956533193f9d4e6841d1979e97d47556fb
chie-hira <capella.a36@gmail.com>
2023-10-28 23:45:31 +0900

    リストで例外処理

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,31 +2,59 @@ package org.example;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-//        クラスをつくって、リストに入れて分岐、例外処理
-//        指定した型しか入らない
+        /* string型のリストを作って例外処理 */
 
         /* Stringをいれてみる */
         List<String> userNames = new ArrayList<>();
 
-        userNames.add("中田");
-        userNames.add("佐藤");
-        userNames.add("浦島");
-        userNames.add("遠藤");
+        userNames.add("マーストン");
+        userNames.add("エセル・ロジャース");
+        userNames.add("トマス・ロジャース");
+        userNames.add("マッカーサー将軍");
+        userNames.add("エミリー・ブレント");
+        userNames.add("ローレンス");
+        userNames.add("アームストロング");
+        userNames.add("ブロア");
+        userNames.add("フィリップ・ロンバート");
+        userNames.add("クレイソーン");
+//        userNames.add(null); //nullは"null"になった
 
-        for (String userName : userNames
-             ) {
-            System.out.println(userName);
+        /* 最初のメンバー */
+        String message = callMember(userNames);
+        System.out.print(message);
+        System.out.println("の" + userNames.size() + "人がいます。");
+        System.out.println("\r");
+
+        /* ランダムで消えていく 誰もいなくなったら処理停止 */
+        for (int i = 0; i < 20; i++) {
+            /* メンバーを並び替え */
+            Collections.shuffle(userNames);
+
+            /* 最初のメンバーが消える */
+            System.out.println(userNames.get(0) + "さんが消えました。");
+            userNames.remove(0);
+            if (!userNames.isEmpty()) {
+                String message2 = callMember(userNames);
+                System.out.print(message2);
+                System.out.println("の" + userNames.size() + "人になりました。");
+                System.out.println("\r");
+            } else {
+                System.out.println("そして誰もいなくなった。");
+                break;
+            }
         }
 
+
         /* クラスを入れてみる */
-        User nakata = new User("中田", 35, "東京都", "登山", LocalDate.of(2000,9,9));
-        User satou = new User("佐藤", 21, "滋賀", "サッカー", LocalDate.of(2000, 2,22));
-        User urashima = new User("浦島", 42, "高知", "釣り", LocalDate.of(2000,10,9));
-        User endo = new User("遠藤", 55, "北海道", "家庭菜園", LocalDate.of(2000, 1,1));
+        User nakata = new User("中田", 35, "東京都", "登山", LocalDate.of(2000, 9, 9));
+        User satou = new User("佐藤", 21, "滋賀", "サッカー", LocalDate.of(2000, 2, 22));
+        User urashima = new User("浦島", 42, "高知", "釣り", LocalDate.of(2000, 10, 9));
+        User endo = new User("遠藤", 55, "北海道", "家庭菜園", LocalDate.of(2000, 1, 1));
 
 //        List<User> users = new ArrayList<>();
 //        users.add(nakata);
@@ -39,16 +67,26 @@ public class Main {
 
         /* 年齢から何年生まれか判定する */
         /* 80年代生まれのuserの趣味を出力する */
-        for (User user : users) {
-            System.out.println(user.getName());
+//        for (User user : users) {
+//            System.out.println(user.getName());
+//        }
+
+        /* クラスをつくってマップに入れて、例外処理 */
+//        hushMapは検索機能に優れている
+    }
+
+    public static String callMember(List<String> userNames) {
+        StringBuilder message = new StringBuilder();
+        message.append("兵隊島には");
+        for (int i = 0; i < userNames.size(); i++) {
+            message.append(userNames.get(i)).append("さん");
+            if (i < userNames.size() - 1) {
+                message.append("、");
+            }
+            if (i == 4) {
+                message.append("\n");
+            }
         }
-
-
-
-
-
-
-//        クラスをつくって、マップに入れる分岐、例外処理
-        System.out.println("Hello world!");
+        return message.toString();
     }
 }
